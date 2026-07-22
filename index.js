2ff72f15c1f7a6f778edf354b515b87b22cb1a4e
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-07-22 10:55:21 +0700

    Memperbaiki format middleware URL-encoded di index.js dan menambahkan file komikController.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded(( extended: true )));
+app.use(express.urlencoded({ extended: true }));
 
 async function startServer() {
     await connectDatabase();
